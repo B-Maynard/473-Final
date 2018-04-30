79a952345cda9dd0403a00f2061c5e45263328ac
Bailey Maynard <bailey_maynard@live.com>
2018-04-29 20:37:10 -0400

    logan's object code

--- final.py
+++ final.py
@@ -1,6 +1,39 @@
 import csv
 
+# Reads csv file and takes into account the first row as the headers for the columns
+
+class MovieInfomation():
+	name = ""
+	director = ""
+	directorFacebookLikes = 0
+	duration = 0
+	leadActor = ""
+	leadActorFacebookLikes = 0
+	gross = 0
+	genres = ""
+	voterNumber = 0
+	contentRating = 0
+	budget = 0
+	year = 0
+	rating = 0
+	totalFacebookLikes = 0
+
+
 with open('movie_metadata.csv', 'rb') as csvfile:
-    moviereader = csv.reader(csvfile, delimiter=',')
+    moviereader = csv.DictReader(csvfile)
+    movies = MovieInfomation()
     for row in moviereader:
-        print row
+    	movies.name = row['movie_title']
+    	movies.director = row['director_name']
+    	movies.directorFacebookLikes = row['director_facebook_likes']
+    	movies.duration = row['duration']
+    	movies.leadActor = row['actor_1_name']
+        movies.leadActorFacebookLikes = row['actor_1_facebook_likes']
+        movies.gross = row['gross']
+        movies.genres = row['genres']
+        movies.voterNumber = row['num_voted_users']
+        movies.contentRating = row['content_rating']
+        movies.budget = row['budget']
+        movies.year = row['title_year']
+        movies.rating = row['imdb_score']
+        movies.totalFacebookLikes = row['movie_facebook_likes']
