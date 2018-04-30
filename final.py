0ca2aa22b5e7ee88ac46924124e19dfee89daf23
FirstBuiltPC <lwenzel90@gmail.com>
2018-04-29 20:37:44 -0400

    Objects fully filled

--- final.py
+++ final.py
@@ -10,7 +10,7 @@ class MovieInfomation():
 	gross = 0
 	genres = ""
 	voterNumber = 0
-	content_rating = 0
+	contentRating = 0
 	budget = 0
 	year = 0
 	rating = 0
@@ -21,11 +21,17 @@ with open('movie_metadata.csv', 'rb') as csvfile:
     moviereader = csv.DictReader(csvfile)
     movies = MovieInfomation()
     for row in moviereader:
-    	movies.name = row['movie_title']
-    	movies.director = row['director_name']
-    	movies.directorFacebookLikes = row['director_facebook_likes']
-    	movies.duration = row['duration']
-    	movies.leadActor = row['actor_1_name']
+		movies.name = row['movie_title']
+		movies.director = row['director_name']
+		movies.directorFacebookLikes = row['director_facebook_likes']
+		movies.duration = row['duration']
+		movies.leadActor = row['actor_1_name']
  		movies.leadActorFacebookLikes = row['actor_1_facebook_likes']
  		movies.gross = row['gross']
- 		movies.genres = row['']
+ 		movies.genres = row['genres']
+ 		movies.voterNumber = row['num_voted_users']
+ 		movies.contentRating = row['content_rating']
+ 		movies.budget = row['budget']
+ 		movies.year = row['title_year']
+ 		movies.rating = row['imdb_score']
+ 		movies.totalFacebookLikes = row['movie_facebook_likes']
