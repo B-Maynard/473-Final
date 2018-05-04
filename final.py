d3185e160eb0197de42dd2d9575ec6963b8ae117
lwenzel90 <lwenzel90@gmail.com>
2018-05-04 11:32:07 -0400

    Added a scatterplot that shows gross vs score

--- final.py
+++ final.py
@@ -1,6 +1,7 @@
 import csv
 from ggplot import *
 import pandas as pd
+import numpy as np
 ########################Import data ########################
 
 #create a dataframe of movies and fill data from csv file
@@ -38,7 +39,12 @@ df['duration'] = df['duration'].fillna(df['duration'].mean().round())
 #fill mising countries with a space
 df['country'] = df['country'].fillna('') 
 
-##########################Visualisation#####################
+#remove movies before 1980 
+df = df.drop(df[df.title_year < 2006].index)
+df = df[np.isfinite(df['title_year'])]
+print df.title_year.to_string()
+
+##########################Visualization#####################
 
 #Plots
 
@@ -46,56 +52,68 @@ df['country'] = df['country'].fillna('')
 # ggtitle("Movie data by year") + xlab("Year") + ylab("Amount") +\
 # geom_histogram(binwidth=.05)
 
-# print yearHist
+#print yearHist
 # yearHist.save('yearHist.png')
-genreDfs = []
 
-actionDf = df[df['genres'].str.contains("Action")]
-adventureDf = df[df['genres'].str.contains("Adventure")]
-animationDf = df[df['genres'].str.contains("Animation")]
-biographyDf = df[df['genres'].str.contains("Biography")]
-comedyDf = df[df['genres'].str.contains("Comedy")]
-crimeDf = df[df['genres'].str.contains("Crime")]
-documentaryDf = df[df['genres'].str.contains("Documentary")]
-dramaDf = df[df['genres'].str.contains("Drama")]
-familyDf = df[df['genres'].str.contains("Family")]
-fantasyDf = df[df['genres'].str.contains("Fantasy")]
-filmNoirDf = df[df['genres'].str.contains("Film-Noir")]
-historyDf = df[df['genres'].str.contains("History")]
-horrorDf = df[df['genres'].str.contains("Horror")]
-musicalDf = df[df['genres'].str.contains("Musical")]
-mysteryDf = df[df['genres'].str.contains("Mystery")]
-newsDf = df[df['genres'].str.contains("News")]
-romanceDf = df[df['genres'].str.contains("Romance")]
-sciFiDf = df[df['genres'].str.contains("Sci-Fi")]
-shortDf = df[df['genres'].str.contains("Short")]
-sportDf = df[df['genres'].str.contains("Sport")]
-thrillerDf = df[df['genres'].str.contains("Thriller")]
-warDf = df[df['genres'].str.contains("War")]
-westernDf = df[df['genres'].str.contains("Western")]
-genres = [
-["Action", actionDf.imdb_score.mean().round(2)],
-["Adventure", adventureDf.imdb_score.mean().round(2)],
-["Animation", animationDf.imdb_score.mean().round(2)],
-["Biography", biographyDf.imdb_score.mean().round(2)],
-["Comedy", comedyDf.imdb_score.mean().round(2)],
-["Crime", crimeDf.imdb_score.mean().round(2)],
-["Documentary", documentaryDf.imdb_score.mean().round(2)],
-["Drama", dramaDf.imdb_score.mean().round(2)],
-["Family", familyDf.imdb_score.mean().round(2)],
-["Fantasy", fantasyDf.imdb_score.mean().round(2)], 
-["Film-Noir", filmNoirDf.imdb_score.mean().round(2)],
-["History", historyDf.imdb_score.mean().round(2)],
-["Horror", horrorDf.imdb_score.mean().round(2)],
-["Musical", musicalDf.imdb_score.mean().round(2)],
-["Mystery", mysteryDf.imdb_score.mean().round(2)],
-["News", newsDf.imdb_score.mean().round(2)],
-["Romance", romanceDf.imdb_score.mean().round(2)],
-["Sci-Fi", sciFiDf.imdb_score.mean().round(2)],
-["Short", shortDf.imdb_score.mean().round(2)],
-["Sport", sportDf.imdb_score.mean().round(2)],
-["Thriller", thrillerDf.imdb_score.mean().round(2)],
-["War", warDf.imdb_score.mean().round(2)],
-["Western", westernDf.imdb_score.mean().round(2)]]
 
-print genres
+# actionDf = df[df['genres'].str.contains("Action")]
+# adventureDf = df[df['genres'].str.contains("Adventure")]
+# animationDf = df[df['genres'].str.contains("Animation")]
+# biographyDf = df[df['genres'].str.contains("Biography")]
+# comedyDf = df[df['genres'].str.contains("Comedy")]
+# crimeDf = df[df['genres'].str.contains("Crime")]
+# documentaryDf = df[df['genres'].str.contains("Documentary")]
+# dramaDf = df[df['genres'].str.contains("Drama")]
+# familyDf = df[df['genres'].str.contains("Family")]
+# fantasyDf = df[df['genres'].str.contains("Fantasy")]
+# filmNoirDf = df[df['genres'].str.contains("Film-Noir")]
+# historyDf = df[df['genres'].str.contains("History")]
+# horrorDf = df[df['genres'].str.contains("Horror")]
+# musicalDf = df[df['genres'].str.contains("Musical")]
+# mysteryDf = df[df['genres'].str.contains("Mystery")]
+# newsDf = df[df['genres'].str.contains("News")]
+# romanceDf = df[df['genres'].str.contains("Romance")]
+# sciFiDf = df[df['genres'].str.contains("Sci-Fi")]
+# shortDf = df[df['genres'].str.contains("Short")]
+# sportDf = df[df['genres'].str.contains("Sport")]
+# thrillerDf = df[df['genres'].str.contains("Thriller")]
+# warDf = df[df['genres'].str.contains("War")]
+# westernDf = df[df['genres'].str.contains("Western")]
+
+
+# genres = [
+# ["Action", actionDf.imdb_score.mean().round(2)],
+# ["Adventure", adventureDf.imdb_score.mean().round(2)],
+# ["Animation", animationDf.imdb_score.mean().round(2)],
+# ["Biography", biographyDf.imdb_score.mean().round(2)],
+# ["Comedy", comedyDf.imdb_score.mean().round(2)],
+# ["Crime", crimeDf.imdb_score.mean().round(2)],
+# ["Documentary", documentaryDf.imdb_score.mean().round(2)],
+# ["Drama", dramaDf.imdb_score.mean().round(2)],
+# ["Family", familyDf.imdb_score.mean().round(2)],
+# ["Fantasy", fantasyDf.imdb_score.mean().round(2)], 
+# ["Film-Noir", filmNoirDf.imdb_score.mean().round(2)],
+# ["History", historyDf.imdb_score.mean().round(2)],
+# ["Horror", horrorDf.imdb_score.mean().round(2)],
+# ["Musical", musicalDf.imdb_score.mean().round(2)],
+# ["Mystery", mysteryDf.imdb_score.mean().round(2)],
+# ["News", newsDf.imdb_score.mean().round(2)],
+# ["Romance", romanceDf.imdb_score.mean().round(2)],
+# ["Sci-Fi", sciFiDf.imdb_score.mean().round(2)],
+# ["Short", shortDf.imdb_score.mean().round(2)],
+# ["Sport", sportDf.imdb_score.mean().round(2)],
+# ["Thriller", thrillerDf.imdb_score.mean().round(2)],
+# ["War", warDf.imdb_score.mean().round(2)],
+# ["Western", westernDf.imdb_score.mean().round(2)]]
+
+# genresDfs = pd.DataFrame.from_records(genres, columns=['genres', 'imdb_score'])
+
+grossScatter = ggplot(df, aes(x='gross', y='imdb_score')) +\
+geom_point() +\
+ylab("IMDB Rating") +\
+xlab("Money Earned") +\
+ggtitle("Rating vs. Gross") +\
+xlim(0,1000000000)
+
+#print grossScatter
+grossScatter.save('grossScatter.png')
